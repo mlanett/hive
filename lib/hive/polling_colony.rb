6d9c37fb0997ca1fbb7a0df93a5a7059104d50d7
Mark Lanett <mark.lanett@gmail.com>
2011-10-16 14:37:18 -0700

    Works well enough.

--- lib/hive/polling_colony.rb
+++ lib/hive/polling_colony.rb
@@ -15,7 +15,7 @@ class Hive::PollingColony
     callable = options[:callable] || callable_block
     timeout  = options[:timeout] || 1024
     
-    collect while running >= 4
+    collect while running >= 40
     
     pid = fork do
       # this is the monitor
@@ -39,23 +39,31 @@ class Hive::PollingColony
     @workers[pid] = Worker.new( pid, start_time, start_time + timeout, 0.125, start_time + 0.125 )
   end
   
-  def check( pid )
-    dummy, status = Process.wait2( pid, Process::WNOHANG )
-    status
+  def check( worker )
   end
   
   def collect()
     # go through all pids
-    pids = []
+    pids    = []
+    now     = Time.now.to_i
+    soonest = now + 1
     @workers.each do |pid, worker|
-      status = check( pid )
-      pids << pid if status
+      next if now < worker.next_check
+      
+      # log "Checking #{worker.pid}"
+      dummy, status = Process.wait2( worker.pid, Process::WNOHANG )
+      pids << worker.pid and next if status
+      
+      worker.window *= 2 if worker.window < 1.0
+      worker.next_check = now + worker.window
+      soonest = worker.next_check if worker.next_check < soonest
     end
     pids.each do |pid|
       @workers.delete(pid)
       @running -= 1
       log "Collected #{pid}; Total Running #{running}"
     end
+    sleep( soonest - now )
   end
   
   def collect_all
