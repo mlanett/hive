598fee25c065f94ad6d60762622b342ce52841e1
Mark Lanett <mark.lanett@gmail.com>
2011-10-15 22:19:42 -0700

    Don't forget!

--- lib/hive.rb
+++ lib/hive.rb
@@ -1,9 +1,11 @@
 require "hive/version"
 
 module Hive
+  autoload :PollingColony, "hive/polling_colony"
   autoload :RedisColony, "hive/redis_colony"
   autoload :SimpleColony, "hive/simple_colony"
   autoload :ThreadedColony, "hive/threaded_colony"
   
+  autoload :Common, "hive/common"
   autoload :Log, "hive/log"
 end
---
+++ lib/hive/redis_colony.rb
@@ -0,0 +1,54 @@
+class Hive::RedisColony
+  
+  include Hive::Log
+  
+  attr :name
+  attr :running
+  attr :pids
+  
+  def initialize( options = {} )
+    @name    = options[:name] || (begin @@cid ||= 0; @@cid += 1; end)
+    @running = 0
+    @pids    = {}
+  end
+  
+  def launch( options = {}, &callable_block )
+    raise if options[:callable] && callable_block
+    callable = options[:callable] || callable_block
+    
+    land while running >= 4
+    pid = fork do
+      log "Worker Starting"
+      callable.call
+      log "Worker Exiting"
+    end
+    @running += 1
+    log "Forked Worker #{pid}; Total Running #{running}"
+    pid
+  end
+  
+  def land()
+    pid = Process.wait
+    @running -= 1
+    log "Landed #{pid}; Total Running #{running}"
+  end
+  
+  def collect_all
+    land while running > 0
+  end
+  
+  include Log
+  
+  class Pid < Struct.new :pid, :start_time, :deadline, :window, :next_check, :last_status
+    def to_s
+      return [
+        "Pid(",
+        pid,
+        Time.now - start_time,
+        last_status
+        ")"
+      ].compact.join(",")
+    end
+  end
+  
+end # Hive::RedisColony
