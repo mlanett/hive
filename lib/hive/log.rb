6963b0f2de0b2c3e9dd96507aa315c5ad0f174bb
Mark Lanett <mark.lanett@gmail.com>
2011-10-15 22:19:57 -0700

    Time in hex is shorter.

--- lib/hive/log.rb
+++ lib/hive/log.rb
@@ -2,7 +2,8 @@ module Hive::Log
   
   def log( *stuffs )
     message = [
-      (Time.now.strftime '%Y%m%d%H%M%S'),
+      #(Time.now.strftime '%Y%m%d%H%M%S'),
+      Time.now.to_i.to_s(16).upcase,
       " [",
       Process.pid,
       (Thread.current[:name] || Thread.current.object_id unless Thread.current == Thread.main),
