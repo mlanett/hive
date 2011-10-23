b496c75ffae3afd288541345cc69acc2f5b5c3fb
Mark Lanett <mark.lanett@gmail.com>
2011-10-23 13:06:59 -0700

    Added dry-run and verbose.

--- lib/hive/configuration.rb
+++ lib/hive/configuration.rb
@@ -53,6 +53,8 @@ class Hive::Configuration
       opts.on( "-p", "--path PATH",   "Add to load path.")            { |d| us.add_path(d) }
       opts.on( "-r", "--require LIB", "Require a library.")           { |l| us.require_lib(l) }
       opts.on( "-s", "--script DSL",  "Include DSL script.")          { |s| us.load_dsl(s) }
+      opts.on( "-v", "--verbose",     "Print stuff out.")             { |s| us.verbose += 1 }
+      opts.on( "--dry-run",           "Don't launch the daemon.")     { us.dry_run = true }
     end.parse!
 
     argv.compact.each { |f| us.load_file(f) }
@@ -65,20 +67,26 @@ class Hive::Configuration
   attr :env
   attr :root
   attr :name, true
+  attr :verbose, true
+  attr :dry_run, true
   
   attr :defaults
   attr :jobs
 
   def initialize()
+    @verbose  = 0
+    @dry_run  = false
     @defaults = {}
     @jobs     = {}
   end
 
   def load_dsl(string)
+    log "Loading #{string}" if verbose >= 2
     instance_eval(string)
   end
 
   def load_file(filename)
+    log "Loading #{filename}" if verbose >= 1
     instance_eval(File.read(filename),filename)
     if ! name then
       n = File.basename(filename).sub(/\.[^.]*$/,'')
@@ -89,13 +97,16 @@ class Hive::Configuration
   def finalize()
     if ! env then
       @env = ( ENV["RAILS_ENV"] || ENV["RACK_ENV"] || "test" )
+      log "Defaulting env to #{env}" if verbose >= 1
     end
     if ! name then
       @name = "hive"
+      log "Defaulting name to #{name}" if verbose >= 1
     end
     if ! @root then
       chdir "/tmp/#{name}"
     end
+    log inspect if verbose >= 2
     freeze
     self
   end
@@ -114,20 +125,23 @@ class Hive::Configuration
   # takes effect immediately
   def chdir(path)
     p = File.expand_path(path)
-    Dir.mkdir(p) if ! Dir.exists?(p)
+    Dir.mkdir(p) if ! Dir.exists?(p) && ! dry_run
     Dir.chdir(p)
+    log "Changed working directory (root) to #{p}" if verbose >= 1
     @root = p
   end
 
   # takes effect immediately
   def add_path(path)
     p = File.expand_path(path)
+    log "Added #{p} to load path" if verbose >= 2
     $:.push(p) unless $:.member?(p)
   end
 
   # convenience for -r on the command line
   def require_lib(r)
     require(r)
+    log "Required #{r}" if verbose >= 2
   end
   
   def set_defaults(options)
@@ -137,6 +151,8 @@ class Hive::Configuration
   def add_pool(name,options)
     options = defaults.merge(options)
     jobs[name] = options
+    log "Added pool for #{name}" if verbose == 1
+    log "Added pool for #{name} with #{options}" if verbose >= 2
   end
 
   def before_fork(&block)
