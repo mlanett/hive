543329b9c81c77a86e8e6a24478253ab37e75b7a
Mark Lanett <mark.lanett@gmail.com>
2011-10-23 00:51:03 -0700

    Worker is partially functional.

---
+++ lib/hive/worker.rb
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+
+=begin
+  
+  A Worker is a forked process which runs jobs.
+  Jobs are short lived and run repeatedly.
+  
+=end
+
+class Hive::Worker
+  
+  # forks a new process
+  # creates a new instance of the job class
+  # runs a loop which calls the job
+  def self.spawn( *arguments )
+    fork do
+      STDIN.reopen "/dev/null"
+      trap("TERM") { quit! }
+      new( *arguments ).run
+    end
+  end
+  
+  attr :policy
+  attr :job_with_feedback
+  attr :job_with_idle
+  attr :state
+  
+  def initialize( options = {}, job, &callable_job )
+    job ||= callable_job
+    @policy            = Hive::Policy.new(options)
+    @job_with_feedback = Hive::Feedback.new( self, job )
+    @job_with_idle     = Hive::Idler.new(@job_with_feedback)
+    @state             = :running
+  end
+  
+  def run()
+    context = { :worker => self }
+    #job_with_feedback.with_feedback do
+      while state == :running do
+        job_with_idle.call( context )
+      end
+    #end
+  end
+
+  def quit!()
+    @state = :quitting
+  end
+  
+end # Hive::Worker
---
+++ spec/worker_spec.rb
@@ -0,0 +1,55 @@
+class Hive::SpawningJob
+  def call( context = {} )
+    redis.set("Hive::SpawningJob",Process.pid)
+    context[:worker].quit
+  end
+  def redis
+    @redis ||= Redis.connect(REDIS)
+  end
+end
+
+class Hive::QuittingJob
+  def call( context = {} )
+    if redis.get("Hive::QuittingJob") == "quit" then
+      context[:worker].quit!
+    else
+      false
+    end
+  end
+  def redis
+    @redis ||= Redis.connect(REDIS)
+  end
+end
+
+describe Hive::Worker do
+  
+  it "should run once" do
+    count  = 0
+    worker = nil
+    job    = ->(context={}) { count += 1; worker.quit! }
+    worker = Hive::Worker.new({},job)
+    worker.run
+    count.must_equal 1
+  end
+
+  it "should pass a context with a worker" do
+    ok     = false
+    worker = nil
+    job    = ->(context={}) { worker.must_equal context[:worker]; worker.quit! }
+    worker = Hive::Worker.new({},job)
+    worker.run
+  end
+
+  it "should spawn a new process" do
+    pid   = Process.pid
+    redis = Redis.connect(REDIS)
+    redis.set "Hive::SpawningJob", pid
+    
+    pid   = Hive::Worker.spawn({},Hive::QuittingJob.new)
+    Hive::Idler.wait_until { redis.get("Hive::SpawningJob") != pid }
+    
+    redis.get("Hive::SpawningJob").wont_equal pid
+    redis.del "Hive::SpawningJob"
+  end
+
+end
