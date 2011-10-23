55207af9757e593917301fdcc7fe370831b9cac6
Mark Lanett <mark.lanett@gmail.com>
2011-10-23 12:44:58 -0700

    Autoload

--- lib/hive.rb
+++ lib/hive.rb
@@ -4,14 +4,17 @@ module Hive
   autoload :Common,         "hive/common"
   autoload :Configuration,  "hive/configuration"
   autoload :Daemon,         "hive/daemon"
+  autoload :Feedback,       "hive/feedback"
   autoload :Idler,          "hive/idler"
   autoload :Log,            "hive/log"
   autoload :Policy,         "hive/policy"
   autoload :PollingColony,  "hive/polling_colony"
+  autoload :Pool,           "hive/pool"
   autoload :ProcessStorage, "hive/process_storage"
   autoload :SignalHook,     "hive/signal_hook"
   autoload :SimpleColony,   "hive/simple_colony"
   autoload :ThreadedColony, "hive/threaded_colony"
+  autoload :Worker,         "hive/worker"
 end
 
 module Hive::Redis
