731f5fa7509622c86e91e2a46ae953b182c8bab4
Mark Lanett <mark.lanett@gmail.com>
2011-11-06 18:31:19 -0800

    Not implemented yet.

--- spec/pool_spec.rb
+++ spec/pool_spec.rb
@@ -15,16 +15,17 @@ describe Hive::Pool do
 
   describe "when spawning proceses", :redis => true do
 
-    it "should spin up two workers" do
-      redis.del "Hive::SpecJob"
-
-      it = Hive::Pool.new( "Hive::SpecJob", Hive::Policy.new(:pool_min_workers => 2, :worker_max_jobs => 1) )
-      it.synchronize
-      Hive::Idler.wait_until { redis.scard("Hive::SpecJob") == 2 }
-
-      redis.scard("Hive::SpecJob").should eq 2
-      redis.del "Hive::SpecJob"
-    end
+    it "should spin up two workers"
+    # do
+    #  redis.del "Hive::SpecJob"
+    #
+    #  it = Hive::Pool.new( "Hive::SpecJob", Hive::Policy.new(:pool_min_workers => 2, :worker_max_jobs => 1) )
+    #  it.synchronize
+    #  Hive::Idler.wait_until { redis.scard("Hive::SpecJob") == 2 }
+    #
+    #  redis.scard("Hive::SpecJob").should eq 2
+    #  redis.del "Hive::SpecJob"
+    #end
 
   end
 
