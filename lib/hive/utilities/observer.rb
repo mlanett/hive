201f757cc20663510143e4f18b34b46d52697fd7
Mark Lanett <mark.lanett@gmail.com>
2011-11-06 19:24:40 -0800

    Oops

--- lib/hive/utilities/observer.rb
+++ lib/hive/utilities/observer.rb
@@ -23,7 +23,7 @@ module Hive::Utilities::Observer
     when String, Symbol
       realize(find_class(candidate))
     else
-      raise "Unknown kind of observer #{potential.inspect}"
+      raise "Unknown kind of observer #{candidate.inspect}"
       return candidate # assume it supports the notifications natively
     end
   end
