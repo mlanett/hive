e73cadc1941661830245a112b464e323857c751d
Mark Lanett <mark.lanett@gmail.com>
2011-11-12 09:09:32 -0800

    Clear out redis between runs.

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 =begin
+@see https://www.relishapp.com/rspec/rspec-core/docs/hooks/around-hooks
 @see https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers/be-matchers
 @see https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers/equality-matchers
 @see https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers/operator-matchers
@@ -10,17 +11,33 @@
 require "bundler/setup"                                                               # set up gem paths
 require "ruby-debug"                                                                  # because sometimes you need it
 require "hive"                                                                        # load this gem
-require "redis"                                                                       # for RedisClient
+require "redis"                                                                       # required by RedisClient
 
 REDIS = { :url => "redis://127.0.0.1:6379/1" }
 
 module RedisClient
   def redis
-    @redis ||= ::Redis.connect(REDIS)
+    @redis ||= begin
+      # debugger
+      ::Redis.connect(REDIS)
+    end
+  end
+  def with_clean_redis(&block)
+    redis.flushall
+    yield
+  ensure
+    redis.flushall
+    redis.quit
   end
 end
 
 RSpec.configure do |spec|
   # @see https://www.relishapp.com/rspec/rspec-core/docs/helper-methods/define-helper-methods-in-a-module
   spec.include RedisClient, :redis => true
+
+  # nuke the Redis database around each run
+  # @see https://www.relishapp.com/rspec/rspec-core/docs/hooks/around-hooks
+  spec.around( :each, :redis => true ) do |example|
+    with_clean_redis { example.run }
+  end
 end
