5cc4e3645b2be7314d9410cf672263d69910384c
Mark Lanett <mark.lanett@gmail.com>
2011-11-12 21:20:05 -0800

    Clean up

--- spec/helper.rb
+++ spec/helper.rb
@@ -8,10 +8,10 @@
 @see https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers/predicate-matchers
 =end
 
-require "bundler/setup"                                                               # set up gem paths
-require "ruby-debug"                                                                  # because sometimes you need it
-require "hive"                                                                        # load this gem
-require "redis"                                                                       # required by RedisClient
+require "bundler/setup"       # set up gem paths
+require "ruby-debug"          # because sometimes you need it
+require "hive"                # load this gem
+require "redis"               # required by RedisClient
 
 REDIS = { :url => "redis://127.0.0.1:6379/1" }
 
