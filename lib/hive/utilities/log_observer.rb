c8a5547061741642bf50999c62683b37a6f797d4
Mark Lanett <mark.lanett@gmail.com>
2011-11-27 22:08:40 -0800

    Clean text

--- lib/hive/utilities/log_observer.rb
+++ lib/hive/utilities/log_observer.rb
@@ -15,19 +15,19 @@ class Hive::Utilities::LogObserver < Hive::Utilities::ObserverBase
   include Hive::Log
 
   def worker_started( *args )
-    log "Worker #{subject} has started"
+    log "#{subject} has started"
   end
   
   def worker_heartbeat( *args )
-    log "Worker #{subject} is still alive"
+    log "#{subject} is still alive"
   end
   
   def job_error(x)
-    log "Warning: Worker #{subject} experienced a job failure due to an error:#{x.inspect}"
+    log "Warning: #{subject} experienced a job failure due to an error:#{x.inspect}"
   end
   
   def worker_stopped( *args )
-    log "Worker #{subject} has stopped"
+    log "#{subject} has stopped"
   end
 
 end # Hive::Utilities::LogObserver
