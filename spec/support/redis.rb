46609de1f94192e0e9599d001aa71048694f1bff
Mark Lanett <mark.lanett@gmail.com>
2011-12-10 14:45:49 -0800

    Clean up

--- spec/support/redis.rb
+++ spec/support/redis.rb
@@ -1,24 +1,22 @@
 # -*- encoding: utf-8 -*-
 
-require "redis"               # required by RedisClient
-
-REDIS = { url: "redis://127.0.0.1:6379/1" }
+require "redis"
 
 module RedisClient
 
+  TEST_REDIS = { url: "redis://127.0.0.1:6379/1" }
+
   def redis
-    @redis ||= begin
-      ::Redis.connect(REDIS)
-    end
+    @redis ||= ::Redis.connect(TEST_REDIS)
   end
 
   def with_clean_redis(&block)
-    redis.flushall
     redis.client.disconnect # auto connect after fork
+    redis.flushall
     yield
   ensure
     redis.flushall
-    redis.quit
+    redis.quit              # quit (close) connection, not server
   end
 
 end # RedisClient
