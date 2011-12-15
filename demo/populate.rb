9ad9fcbd435a0fc076a2c241f1ce1db1a5a133a3
Mark Lanett <mark.lanett@gmail.com>
2011-12-14 22:31:42 -0800

    Helper for job3

---
+++ demo/populate.rb
@@ -0,0 +1,21 @@
+require "hive"
+require "hive/squiggly"
+require "redis"
+
+redis   = Redis.connect url: "redis://127.0.0.1:6379/0"
+storage = Hive::Redis::Storage.new(redis)
+
+storage.del "Names"
+storage.del "Activity"
+storage.del "Weight"
+storage.del "Next"
+
+(1..1000).each do |i|
+  name     = Squiggly.subject
+  activity = rand(1000)
+
+  storage.map_set "Names", "Page-#{i}", name
+  storage.map_set "Activity", "Page-#{i}", activity
+
+  storage.queue_add( "Next", "Page-#{i}", 0 )
+end
